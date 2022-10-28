c7151f7b851f22c13c80668d8222c1940176fd55
ioannis Apostolou <giannistolou@protonmail.com>
2022-10-28 18:14:58 +0300

    feat: add new icon
    also add docker restart always

--- bookCafe/settings.py
+++ bookCafe/settings.py
@@ -130,6 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploadsfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "dist",
+    BASE_DIR / "public",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
