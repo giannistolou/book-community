5c574c87c91df02be77081ea9332ed5951ff4981
ioannis Apostolou <giannistolou@protonmail.com>
2023-01-01 19:18:36 +0200

    fix: errors on subdomains

--- bookCommunity/hosts.py
+++ bookCommunity/hosts.py
@@ -2,6 +2,6 @@ from django_hosts import patterns, host
 from django.conf import settings
 
 host_patterns = patterns('',
-    host(r'', settings.ROOT_URLCONF, name='landing page'),
+    host(r'^$', settings.ROOT_URLCONF, name='www'),
     host(r'cafe', 'findBookCafe.urls', name='cafe'),
 )
--- bookCommunity/urls.py
+++ bookCommunity/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('landingPage.urls')),
+    path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'findBookCafe.views.page404'
