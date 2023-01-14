2102334f618e9fc968cf5879acdcd7d91b5ece44
ioannis Apostolou <giannistolou@protonmail.com>
2023-01-14 22:47:51 +0200

    feat: env support many domains

--- bookCommunity/settings.py
+++ bookCommunity/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
 
 CSRF_TRUSTED_ORIGINS = ['https://cafe.book-community.com', 'https://www.book-community.com', 'https://book-community.com']
 
