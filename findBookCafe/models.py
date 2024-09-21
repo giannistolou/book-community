121427afead20a0946e3df4d1f0cba512e4e90c2
ioannis Apostolou <giannistolou@protonmail.com>
2024-09-21 14:44:12 +0300

    order_position can be null

--- findBookCafe/models.py
+++ findBookCafe/models.py
@@ -61,7 +61,7 @@ class Shop(models.Model):
 	description = QuillField()
 	created_date = models.DateField(auto_now_add = True)
 	updated_on = models.DateField(auto_now= True)
-	order_position = models.IntegerField(default=0)
+	order_position = models.IntegerField(default=0, blank=True, null=True)
 	
 	def __str__(self):
 		return str(self.id) + ' ' + self.name + ' ' + self.type
@@ -86,7 +86,7 @@ class Collection(models.Model):
 	updated_on = models.DateField(auto_now= True)
 	thumbnail = models.ImageField(upload_to ='uploads/', null = True)
 	shops = models.ManyToManyField(Shop)
-	order_position = models.IntegerField(default=0)
+	order_position = models.IntegerField(default=0, blank=True, null=True)
 	
 	def __str__(self):
 		return str(self.title)
