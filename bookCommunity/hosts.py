e43803406252be9df665b2b75988828b69c1655b
ioannis Apostolou <giannistolou@protonmail.com>
2024-10-07 23:58:23 +0300

    small changes

--- bookCommunity/hosts.py
+++ bookCommunity/hosts.py
@@ -4,5 +4,6 @@ from django.conf import settings
 host_patterns = patterns('',
     host(r'^$', settings.ROOT_URLCONF, name='www'),
     host(r'cafe', 'findBookCafe.urls', name='cafe'),
-    host(r'blog', 'blog.urls', name='blog'),
+    # TODO change to blog
+    host(r'blog2', 'blog.urls', name='blog'),
 )
