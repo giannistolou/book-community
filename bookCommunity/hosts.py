c9e838aef5903328478feea2727b44fcb2645bc2
Ioannis Apostolou <hey@giannistolou.gr>
2024-11-11 13:54:37 +0200

    add admin site url

--- bookCommunity/hosts.py
+++ bookCommunity/hosts.py
@@ -1,9 +1,10 @@
 from django_hosts import patterns, host
 from django.conf import settings
+from django.contrib import admin
 
 host_patterns = patterns('',
     host(r'^$', settings.ROOT_URLCONF, name='www'),
     host(r'cafe', 'findBookCafe.urls', name='cafe'),
-    # TODO change to blog
     host(r'blog', 'blog.urls', name='blog'),
+    host(r'admin',  admin.site.urls, name='admin'),
 )
