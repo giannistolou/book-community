d84e94fc8035418e18b3d23fa98ac11c87aaaab3
Ioannis Apostolou <hey@giannistolou.gr>
2025-02-27 21:41:49 +0200

    feat: change editor

--- bookCommunity/urls.py
+++ bookCommunity/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from prose import views
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('landingPage.urls')),
+    path("attachment/", views.upload_attachment, name="prose_upload_attachment"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'findBookCafe.views.page404'
