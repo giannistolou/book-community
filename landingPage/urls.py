b1a94b9a1fbfceef9404d6ffe2e6dc53460133c0
Ioannis Apostolou <hey@giannistolou.gr>
2025-06-12 01:28:51 +0300

    Add bio page

--- landingPage/urls.py
+++ landingPage/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('bio/', views.bio, name='bio'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- landingPage/views.py
+++ landingPage/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import Http404
+from findBookCafe.models import Collection  
+from blog.models import BlogPost  
+
 # Create your views here.
 def index(request):
 	return render(request, 'landing-page.html')
+
+def bio(request):
+	latest_collections = Collection.objects.all().order_by('order_position')[:2]
+	latest_posts = BlogPost.objects.order_by('-published_at')[:2]
+	
+	return render(request, 'bio.html', {
+        'latest_collections': latest_collections,
+        'latest_posts': latest_posts,
+    })
