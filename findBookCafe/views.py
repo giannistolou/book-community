93fead00fa4f8824fb1d4bf00c4a7d5cde7ae203
Giannis Apostolou <hey@giannistolou.gr>
2026-04-16 19:48:49 +0300

    add filter icon to toggle suggestions cafes

--- findBookCafe/views.py
+++ findBookCafe/views.py
@@ -103,7 +103,7 @@ def map(request):
             path_type = 'libraries'
         path = "/" + path_type + '/' + shop.region.city.slug + '/' + shop.region.slug
         thumbnail = str(shop.thumbnail) if shop.thumbnail else ''
-        data.append({'name': shop.name, 'slug': shop.slug, 'id': shop.id, 'directions': shop.googleMaps, 'path': path, 'shopType': shop_type, 'thumbnail': thumbnail, 'type': "Feature", 'properties': {'iconSize': [60, 60]}, 'geometry': {'type': "Point", 'coordinates': [shop.longitude, shop.latitude]}, 'address': f"{shop.region.name}, {shop.address}"})
+        data.append({'name': shop.name, 'slug': shop.slug, 'id': shop.id, 'directions': shop.googleMaps, 'path': path, 'shopType': shop_type, 'thumbnail': thumbnail, 'type': "Feature", 'properties': {'iconSize': [60, 60]}, 'geometry': {'type': "Point", 'coordinates': [shop.longitude, shop.latitude]}, 'address': f"{shop.address}"})
     return render(request, 'map.html', {'shops': data})
 
 def simple_page(request, page_slug):
